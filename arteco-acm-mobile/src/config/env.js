c59536b849aa09f7f2dada362246d23ad7df5842
Carson <carson@arteco-system.com>
2026-01-16 10:31:12 +0000

    Update acm mobile file for  working version

--- arteco-acm-mobile/src/config/env.js
+++ arteco-acm-mobile/src/config/env.js
@@ -1,4 +1,2 @@
-import { IS_PRODUCTION, API_URL } from '@env';
-
 export const BASE_URL = "https://arteco-fineartapi-prod-bxetekage3a2b6em.eastus2-01.azurewebsites.net/api";
-export { IS_PRODUCTION };
+export const IS_PRODUCTION = true;
