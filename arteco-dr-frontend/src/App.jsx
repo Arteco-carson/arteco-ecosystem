d009a3e0a1c18a80fb3edf2735a94375bbb4d4fb
Carson <carson@arteco-system.com>
2026-01-18 13:20:43 +0000

    update app.js to fix build

--- arteco-dr-frontend/src/App.jsx
+++ arteco-dr-frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider, useAuth } from './context/AuthContext';
-import Login from './pages/Login';
+import Login from './pages/login';
 import Dashboard from './pages/Dashboard';
 import ReportList from './pages/ReportList';
 import 'antd/dist/reset.css';
